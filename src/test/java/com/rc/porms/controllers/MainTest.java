e33714ff677e1518ec2e99693b0d5b8d5b796b4d
unknown <conradmartinez050@gmail.com>
2024-12-13 20:13:05 +0800

    RCPS-173 desktop-automation-testing: Automation Testing of Login Module

---
+++ src/test/java/com/rc/porms/controllers/MainTest.java
@@ -0,0 +1,140 @@
+package com.rc.porms.controllers;
+
+import com.rc.porms.controllers.main.MainController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.TestFx;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.isVisible;
+
+
+class MainTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(getClass().getResource("/views/MainView.fxml"));
+
+        BorderPane mainLayout = new BorderPane();
+        mainLayout = loader.load();
+
+        Scene scene = new Scene(mainLayout);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @TestFx
+    public void testLoginValidation() {
+        MainController.setTestMode(true);
+
+        //testing kung ang username ay tumatanggap ng may alpha numeric
+        clickOn("#usernameField");
+        write("conrad()");
+        clickOn("#passwordField");
+        write("Password@123");
+        clickOn("#logButton");
+        sleep(2000);
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        clickOn("#usernameField");
+        eraseText(8);
+        clickOn("#passwordField");
+        eraseText(12);
+
+        //testing kung ang password ay tumatanggap ng walang special character
+        clickOn("#usernameField");
+        write("conrad");
+        clickOn("#passwordField");
+        write("Password123");
+        clickOn("#logButton");
+        sleep(2000);
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        clickOn("#usernameField");
+        eraseText(6);
+        clickOn("#passwordField");
+        eraseText(11);
+
+        //testing kung ang makaka log in if walang ilagay sa username and password
+        clickOn("#logButton");
+        sleep(2000);
+
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        //testing kung ang makaka log in if walang ilagay sa username
+        clickOn("#passwordField");
+        write("Password@123");
+        clickOn("#logButton");
+        sleep(2000);
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        clickOn("#passwordField");
+        eraseText(12);
+
+        //testing kung ang makaka log in if walang ilagay sa password
+        clickOn("#usernameField");
+        write("conrad");
+        clickOn("#logButton");
+        sleep(2000);
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        clickOn("#usernameField");
+        eraseText(6);
+
+
+        //testing kung makaka login if mali yung username tas tama yung password
+        clickOn("#usernameField");
+        write("wrongusername");
+        clickOn("#passwordField");
+        write("Password@123");
+        clickOn("#logButton");
+        sleep(2000);
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        clickOn("#usernameField");
+        eraseText(13);
+        clickOn("#passwordField");
+        eraseText(12);
+
+        //testing kung makaka log in kahit mali yung password
+        clickOn("#usernameField");
+        write("conrad");
+        clickOn("#passwordField");
+        write("Wrongpass@123");
+        clickOn("#eyeBrow");
+        sleep(1000);
+        clickOn("#eyeBrow");
+        clickOn("#logButton");
+        sleep(2000);
+        verifyThat(".dialog-pane", isVisible());
+        clickOn("OK");
+
+        clickOn("#usernameField");
+        eraseText(6);
+        clickOn("#passwordField");
+        eraseText(13);
+
+        //test kung makaka log in if tama yung credentials
+        clickOn("#usernameField");
+        write("conrad");
+        clickOn("#passwordField");
+        write("Password@123");
+        clickOn("#logButton");
+        sleep(2000);
+    }
+
+}
