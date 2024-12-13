7a19ebb859080acd7d928e18ec7b0db32de00b46
Josh <joshuagerome.roque@gmail.com>
2024-12-13 22:05:26 +0800

    RCPS-238: desktop-unit-testing: Implement Unit Testing for EmployeeFacadeImpl

--- src/main/test/com/rc/porms/appl/facade/employee/impl/EmployeeFacadeImplTest.java
+++ src/main/test/com/rc/porms/appl/facade/employee/impl/EmployeeFacadeImplTest.java
@@ -18,7 +18,6 @@ class EmployeeFacadeImplTest {
     @Test
     void testGetAllEmployees() {
         Employee employee1 = new Employee();
-<<<<<<< HEAD
         employee1.setEmployeeNo("EMP16-0005");
         employee1.setLastName("Roque");
         employee1.setFirstName("Joshua");
@@ -27,16 +26,6 @@ class EmployeeFacadeImplTest {
         employee2.setEmployeeNo("EMP16-0143");
         employee2.setLastName("Claus");
         employee2.setFirstName("Saint");
-=======
-        employee1.setEmployeeNo("EMP21-0143");
-        employee1.setLastName("Magnaye");
-        employee1.setFirstName("Justine");
-
-        Employee employee2 = new Employee();
-        employee2.setEmployeeNo("EMP21-0144");
-        employee2.setLastName("Doe");
-        employee2.setFirstName("John");
->>>>>>> a2576df5d6196d9d7e42ffb197218cf85ee53497
 
         List<Employee> employees = Arrays.asList(employee1, employee2);
         when(employeeDao.getAllEmployees()).thenReturn(employees);
@@ -50,19 +39,11 @@ class EmployeeFacadeImplTest {
 
     @Test
     void testGetEmployeeById() {
-<<<<<<< HEAD
         String employeeId = "EMP16-0005";
         Employee expectedEmployee = new Employee();
         expectedEmployee.setEmployeeNo(employeeId);
         expectedEmployee.setLastName("Roque");
         expectedEmployee.setFirstName("Joshua");
-=======
-        String employeeId = "EMP21-0143";
-        Employee expectedEmployee = new Employee();
-        expectedEmployee.setEmployeeNo(employeeId);
-        expectedEmployee.setLastName("Magnaye");
-        expectedEmployee.setFirstName("Justine");
->>>>>>> a2576df5d6196d9d7e42ffb197218cf85ee53497
         when(employeeDao.getEmployeeById(employeeId)).thenReturn(expectedEmployee);
 
         Employee result = employeeFacade.getEmployeeById(employeeId);
@@ -76,15 +57,9 @@ class EmployeeFacadeImplTest {
     void testGetById() {
         int employeeId = 1;
         Employee expectedEmployee = new Employee();
-<<<<<<< HEAD
         expectedEmployee.setEmployeeNo("EMP16-0005");
         expectedEmployee.setLastName("Roque");
         expectedEmployee.setFirstName("Joshua");
-=======
-        expectedEmployee.setEmployeeNo("EMP21-0143");
-        expectedEmployee.setLastName("Magnaye");
-        expectedEmployee.setFirstName("Justine");
->>>>>>> a2576df5d6196d9d7e42ffb197218cf85ee53497
         when(employeeDao.getById(employeeId)).thenReturn(expectedEmployee);
 
         Employee result = employeeFacade.getById(employeeId);
@@ -97,15 +72,9 @@ class EmployeeFacadeImplTest {
     @Test
     void testAddEmployee() {
         Employee newEmployee = new Employee();
-<<<<<<< HEAD
         newEmployee.setEmployeeNo("EMP16-0144");
         newEmployee.setLastName("Claus");
         newEmployee.setFirstName("Rudolf");
-=======
-        newEmployee.setEmployeeNo("EMP21-0145");
-        newEmployee.setLastName("Smith");
-        newEmployee.setFirstName("Emily");
->>>>>>> a2576df5d6196d9d7e42ffb197218cf85ee53497
 
         when(employeeDao.addEmployee(newEmployee)).thenReturn(true);
         when(employeeDao.getEmployeeById(newEmployee.getEmployeeNo())).thenReturn(null);
@@ -119,7 +88,6 @@ class EmployeeFacadeImplTest {
     @Test
     void testUpdateEmployee() {
         Employee existingEmployee = new Employee();
-<<<<<<< HEAD
         existingEmployee.setEmployeeNo("EMP16-0005");
         existingEmployee.setLastName("Roque");
         existingEmployee.setFirstName("Joshua");
@@ -128,16 +96,6 @@ class EmployeeFacadeImplTest {
         updatedEmployee.setEmployeeNo("EMP16-0005");
         updatedEmployee.setLastName("Roque");
         updatedEmployee.setFirstName("Joshua Gerome");
-=======
-        existingEmployee.setEmployeeNo("EMP21-0143");
-        existingEmployee.setLastName("Magnaye");
-        existingEmployee.setFirstName("Justine");
-
-        Employee updatedEmployee = new Employee();
-        updatedEmployee.setEmployeeNo("EMP21-0143");
-        updatedEmployee.setLastName("Magnaye");
-        updatedEmployee.setFirstName("Justine Updated");
->>>>>>> a2576df5d6196d9d7e42ffb197218cf85ee53497
 
         when(employeeDao.getEmployeeById(existingEmployee.getEmployeeNo())).thenReturn(existingEmployee);
         when(employeeDao.updateEmployee(updatedEmployee)).thenReturn(true);
